92c5a2965fb4085f80075f8268316f30bafb7676
Lazurus82 <42788428+Lazurus82@users.noreply.github.com>
2018-09-23 15:49:53 -0500

    Add files via upload

---
+++ Foundation/wilbur/week1/Homework1/DownHillBike.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace HelloWorld.Homework1
+{
+    public class DownHillBike : Mountainbike
+    {
+
+        public override int Reflectors { get; } = 7;
+
+
+        public override int Brakes { get; } = 2;
+
+
+    }
+}
+
